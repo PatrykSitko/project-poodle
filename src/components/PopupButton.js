7f15dee55637a2988ffb099ba868929c6daf30f5
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-11 19:48:44 +0200

    Added .popup(-close)-button-mousedown classes.

--- src/components/PopupButton.js
+++ src/components/PopupButton.js
@@ -2,22 +2,36 @@ import React, { useState } from "react";
 import "./PopupButton.css";
 export default function PopupButton({ style, content, children }) {
   const [clicked, setClicked] = useState(false);
+  const [popupCloseButtonClassName, setPopupCloseButtonClassName] = useState(
+    ""
+  );
+  const [popupButtonClassName, setPopupButtonClassName] = useState("");
   return [
     clicked && (
       <div className="popup-window" key="popup-window" style={style}>
         <div className="popup-content">{content}</div>
         <h3
-          className="popup-close-button"
-          onClick={event => setClicked(!clicked)}
+          className={"popup-close-button " + popupCloseButtonClassName}
+          onMouseDown={event => {
+            setPopupCloseButtonClassName("popup-close-button-mousedown");
+          }}
+          onMouseUp={event => {
+            setClicked(!clicked);
+            setPopupCloseButtonClassName("");
+          }}
         >
           Close
         </h3>
       </div>
     ),
     <button
-      className="popup-button"
+      className={"popup-button " + popupButtonClassName}
       key="popup-button"
-      onClick={event => setClicked(!clicked)}
+      onMouseDown={__ => setPopupButtonClassName("popup-button-mousedown")}
+      onMouseUp={__ => {
+        setClicked(!clicked);
+        setPopupButtonClassName("");
+      }}
     >
       {children}
     </button>
