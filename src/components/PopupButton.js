69176c40d75b1f292cc657f70c702469f81cffd5
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-11 21:52:11 +0200

    Fixed a bug where the button would not be reassigned with the mousedown css classes when the user leaves and enters the button with a triggered mousedown event

--- src/components/PopupButton.js
+++ src/components/PopupButton.js
@@ -5,7 +5,12 @@ export default function PopupButton({ style, content, children }) {
   const [popupCloseButtonClassName, setPopupCloseButtonClassName] = useState(
     ""
   );
+  const [
+    popupCloseButton_mouseIsDown,
+    setMouseIsDown_popupCloseButton
+  ] = useState(false);
   const [popupButtonClassName, setPopupButtonClassName] = useState("");
+  const [popupButton_mouseIsDown, setMouseIsDown_popupButton] = useState(false);
   return [
     clicked && (
       <div className="popup-window" key="popup-window" style={style}>
@@ -13,12 +18,19 @@ export default function PopupButton({ style, content, children }) {
         <h3
           className={"popup-close-button " + popupCloseButtonClassName}
           onMouseDown={event => {
+            setMouseIsDown_popupCloseButton(true);
             setPopupCloseButtonClassName("popup-close-button-mousedown");
           }}
           onMouseUp={event => {
-            setClicked(!clicked);
+            setMouseIsDown_popupCloseButton(false);
             setPopupCloseButtonClassName("");
+            setClicked(!clicked);
           }}
+          onMouseLeave={__ => setPopupCloseButtonClassName("")}
+          onMouseEnter={__ =>
+            popupCloseButton_mouseIsDown &&
+            setPopupCloseButtonClassName("popup-close-button-mousedown")
+          }
         >
           Close
         </h3>
@@ -27,11 +39,20 @@ export default function PopupButton({ style, content, children }) {
     <button
       className={"popup-button " + popupButtonClassName}
       key="popup-button"
-      onMouseDown={__ => setPopupButtonClassName("popup-button-mousedown")}
-      onMouseUp={__ => {
-        setClicked(!clicked);
-        setPopupButtonClassName("");
+      onMouseDown={__ => {
+        setMouseIsDown_popupButton(true);
+        setPopupButtonClassName("popup-button-mousedown");
       }}
+      onMouseUp={__ => {
+        setMouseIsDown_popupButton(false);
+        setPopupButtonClassName("");
+        setClicked(!clicked);
+      }}
+      onMouseLeave={__ => setPopupButtonClassName("")}
+      onMouseEnter={__ =>
+        popupButton_mouseIsDown &&
+        setPopupButtonClassName("popup-button-mousedown")
+      }
     >
       {children}
     </button>
