747e60eb3fa9b71d544746f5bf4ad7b27ab12829
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-11 22:10:25 +0200

    Changed Student.js to export by default an array of React Components instead of a funcion that returns that array.

--- src/components/views/Student.js
+++ src/components/views/Student.js
@@ -3,10 +3,8 @@ import Grades from "../Grades";
 import UserInfo from "../UserInfo";
 import Logout from "../LogoutButton";
 
-export default function Student() {
-  return [
+export default [
     <Logout key="Logout" />,
     <UserInfo key="UserInfo" />,
     <Grades key="Grades" />
   ];
-}
