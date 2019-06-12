6389fb97f573b3df02f930dedf98082a89ad544e
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-12 02:00:00 +0200

    Added mousedown/up eventListener

--- src/redux/eventListeners.js
+++ src/redux/eventListeners.js
@@ -1,10 +1,18 @@
-import { setWidth, setHeight } from "./actions";
+import * as action from "./actions";
 
 export function bindEventListeners(store) {
   window.addEventListener("resize", event => {
     const width = window.innerWidth;
     const height = window.innerHeight;
-    if (store.getState().width !== width) store.dispatch(setWidth(width));
-    if (store.getState().height !== height) store.dispatch(setHeight(height));
+    if (store.getState().width !== width)
+      store.dispatch(action.setWidth(width));
+    if (store.getState().height !== height)
+      store.dispatch(action.setHeight(height));
+  });
+  window.addEventListener("mousedown", event => {
+    store.dispatch(action.setMousedown(true));
+  });
+  window.addEventListener("mouseup", event => {
+    store.dispatch(action.setMousedown(false));
   });
 }
