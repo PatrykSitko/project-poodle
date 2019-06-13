80751e7731aaa2e3555d0ed2849364dca1d980bc
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-13 08:33:18 +0200

    Modified imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import ShowLoginScreen from "./components/LoginForm";
-import LoadView from "./components/ViewLoader";
+import ShowLoginScreen from "./components/login";
+import LoadView from "./components/view";
 import { connect } from "react-redux";
 import "./App.css";
 
-function props({ token, url }) {
+function props({ session }) {
   return {
-    isConnectedThenLoadView: () => token && url && <LoadView />
+    isConnectedThenLoadView: () =>
+      session && session.token && session.url && <LoadView />
   };
 }
 
