9582c30ec56638cee696980c051db8a1b50bcc53
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-18 01:49:55 +0200

    Created a groups component inside of pages/instructor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { browserHistory } from "./redux/store/configuration";
 import { Router, Switch, Route } from "react-router-dom";
 import Login from "./components/login";
 import Student from "./pages/student";
-import Groups from "./components/group";
+import Groups from "./pages/instructor/groups";
 import { connect } from "react-redux";
 import React from "react";
 import "./App.css";
---
+++ src/pages/instructor/groups.js
@@ -0,0 +1,126 @@
+import React from "react";
+import Group from "../../components/group";
+import Member from "../../components/group/member";
+import { connect } from "react-redux";
+
+const mapStateToProps = ({ windowInnerWidth: { value } }) => {
+  return { widnowInnerWidth: value };
+};
+
+export function Groups({ widnowInnerWidth }) {
+  return (
+    <Group
+      globalStyle={{ margin: 3, padding: 3 }}
+      /* columns={[[1, 500], [2, 1000], [3, 1500]]} */
+      columns={Math.floor(widnowInnerWidth / 500)}
+      filters={[member => member.props.children !== "hello"]}
+    >
+      <Member key="1">hello1</Member>
+      <Member key="2">hello</Member>
+      <Member key="3">hello</Member>
+      <Member key="4">hello</Member>
+      <Member key="5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus
+        lacinia turpis, a laoreet libero finibus vel. Suspendisse ultrices orci
+        vitaeLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        maximus lacinia turpis, a laoreet libero finibus vel. Suspendisse
+        ultrices orci vitaeLorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Sed maximus lacinia turpis, a laoreet libero finibus vel.
+        Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed maximus lacinia turpis, a laoreet libero finibus
+        vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitae metus suscipit
+        ullamcorper. Aliquam suscipit sapien ac dui maximus porta. Nam gravida
+        hendrerit ullamcorper. Morbi semper finibus nibh, eu ornare turpis
+        sagittis condimentum. Proin ut nisi tortor. Aliquam congue erat eget ex
+        sodales consectetur. Nam vel diam sit amet nisl fermentum vulputate nec
+        eget neque. Etiam molestie urna porttitor lectus tristique egestas. Nam
+        convallis finibus porta. Donec est justo, ultrices non orci et, dapibus
+        tincidunt eros. Aenean ullamcorper sem vel risus maximus ultrices. Sed
+        porttitor magna justo, a pulvinar velit tincidunt finibus. Vestibulum
+        sem augue, posuere id porta sit amet, aliquam id sapien. Nunc mollis
+        nibh sed blandit faucibus. Duis venenatis, ante non molestie
+      </Member>
+      <Member key="6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus
+        lacinia turpis, a laoreet libero finibus vel. Suspendisse ultrices orci
+        vitaeLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        maximus lacinia turpis, a laoreet libero finibus vel. Suspendisse
+        ultrices orci vitaeLorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Sed maximus lacinia turpis, a laoreet libero finibus vel.
+        Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed maximus lacinia turpis, a laoreet libero finibus
+        vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitae metus suscipit
+        ullamcorper. Aliquam suscipit sapien ac dui maximus porta. Nam gravida
+        hendrerit ullamcorper. Morbi semper finibus nibh, eu ornare turpis
+        sagittis condimentum. Proin ut nisi tortor. Aliquam congue erat eget ex
+        sodales consectetur. Nam vel diam sit amet nisl fermentum vulputate nec
+        eget neque. Etiam molestie urna porttitor lectus tristique egestas. Nam
+        convallis finibus porta. Donec est justo, ultrices non orci et, dapibus
+        tincidunt eros. Aenean ullamcorper sem vel risus maximus ultrices. Sed
+        porttitor magna justo, a pulvinar velit tincidunt finibus. Vestibulum
+        sem augue, posuere id porta sit amet, aliquam id sapien. Nunc mollis
+        nibh sed blandit faucibus. Duis venenatis, ante non molestie
+      </Member>
+      <Member key="7">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus
+        lacinia turpis, a laoreet libero finibus vel. Suspendisse ultrices orci
+        vitaeLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        maximus lacinia turpis, a laoreet libero finibus vel. Suspendisse
+        ultrices orci vitaeLorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Sed maximus lacinia turpis, a laoreet libero finibus vel.
+        Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed maximus lacinia turpis, a laoreet libero finibus
+        vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitae metus suscipit
+        ullamcorper. Aliquam suscipit sapien ac dui maximus porta. Nam gravida
+        hendrerit ullamcorper. Morbi semper finibus nibh, eu ornare turpis
+        sagittis condimentum. Proin ut nisi tortor. Aliquam congue erat eget ex
+        sodales consectetur. Nam vel diam sit amet nisl fermentum vulputate nec
+        eget neque. Etiam molestie urna porttitor lectus tristique egestas. Nam
+        convallis finibus porta. Donec est justo, ultrices non orci et, dapibus
+        tincidunt eros. Aenean ullamcorper sem vel risus maximus ultrices. Sed
+        porttitor magna justo, a pulvinar velit tincidunt finibus. Vestibulum
+        sem augue, posuere id porta sit amet, aliquam id sapien. Nunc mollis
+        nibh sed blandit faucibus. Duis venenatis, ante non molestie
+      </Member>
+      <Member key="8">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed maximus
+        lacinia turpis, a laoreet libero finibus vel. Suspendisse ultrices orci
+        vitaeLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        maximus lacinia turpis, a laoreet libero finibus vel. Suspendisse
+        ultrices orci vitaeLorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Sed maximus lacinia turpis, a laoreet libero finibus vel.
+        Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed maximus lacinia turpis, a laoreet libero finibus
+        vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitaeLorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Sed maximus lacinia turpis, a laoreet
+        libero finibus vel. Suspendisse ultrices orci vitae metus suscipit
+        ullamcorper. Aliquam suscipit sapien ac dui maximus porta. Nam gravida
+        hendrerit ullamcorper. Morbi semper finibus nibh, eu ornare turpis
+        sagittis condimentum. Proin ut nisi tortor. Aliquam congue erat eget ex
+        sodales consectetur. Nam vel diam sit amet nisl fermentum vulputate nec
+        eget neque. Etiam molestie urna porttitor lectus tristique egestas. Nam
+        convallis finibus porta. Donec est justo, ultrices non orci et, dapibus
+        tincidunt eros. Aenean ullamcorper sem vel risus maximus ultrices. Sed
+        porttitor magna justo, a pulvinar velit tincidunt finibus. Vestibulum
+        sem augue, posuere id porta sit amet, aliquam id sapien. Nunc mollis
+        nibh sed blandit faucibus. Duis venenatis, ante non molestie
+      </Member>
+    </Group>
+  );
+}
+
+export default connect(mapStateToProps)(Groups);
