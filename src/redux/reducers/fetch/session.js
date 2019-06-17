fa14c6d1b9d1768f9dd8db994ee8dec3b9b2fac7
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-17 02:42:06 +0200

    Added more action types.

--- src/redux/reducers/fetch/session.js
+++ src/redux/reducers/fetch/session.js
@@ -1,6 +1,18 @@
-import { SESSION_ESTABLISHED } from "../../actions/types";
+import {
+  ESTABLISHING_SESSION,
+  SESSION_ESTABLISHED,
+  FAILED_ESTABLISHING_SESSION
+} from "../../actions/types";
 
-export const initialSessionState = { url: undefined, token: undefined };
+export const initialSessionState = { token: undefined };
 
-export default (state = initialSessionState, { type: action, payload }) =>
-  action === SESSION_ESTABLISHED ? { ...state, ...payload } : state;
+export default (state = initialSessionState, { type: action, payload }) => {
+  switch (action) {
+    default:
+    case FAILED_ESTABLISHING_SESSION:
+    case ESTABLISHING_SESSION:
+      return state;
+    case SESSION_ESTABLISHED:
+      return { ...state, ...payload };
+  }
+};
