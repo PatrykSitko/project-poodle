681b06d2233c68f3ab931eb0371824017e369308
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-17 15:27:03 +0200

    changed default export to arrow function

--- src/redux/actions/fetch/student.js
+++ src/redux/actions/fetch/student.js
@@ -7,18 +7,17 @@ export function setStudent(student) {
   };
 }
 
-export default () =>
-  async function(dispatch, getState) {
-    const url = await getState().router.pathname;
-    const token = await getState().session.token;
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ token })
-    });
-    const data = await response.json();
-    console.log(data);
-    dispatch(setStudent(data));
-  };
+export default () => async (dispatch, getState) => {
+  const url = await getState().router.pathname;
+  const token = await getState().session.token;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ token })
+  });
+  const data = await response.json();
+  console.log(data);
+  dispatch(setStudent(data));
+};
