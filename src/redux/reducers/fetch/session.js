911edcaa3e6a4a2e1073a041c82662b081fbaa7c
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-17 15:27:48 +0200

    Added type LOGGED_OUT

--- src/redux/reducers/fetch/session.js
+++ src/redux/reducers/fetch/session.js
@@ -1,7 +1,8 @@
 import {
   ESTABLISHING_SESSION,
   SESSION_ESTABLISHED,
-  FAILED_ESTABLISHING_SESSION
+  FAILED_ESTABLISHING_SESSION,
+  LOGGED_OUT
 } from "../../actions/types";
 
 export const initialSessionState = { token: undefined };
@@ -9,10 +10,11 @@ export const initialSessionState = { token: undefined };
 export default (state = initialSessionState, { type: action, payload }) => {
   switch (action) {
     default:
-    case FAILED_ESTABLISHING_SESSION:
     case ESTABLISHING_SESSION:
+    case FAILED_ESTABLISHING_SESSION:
       return state;
     case SESSION_ESTABLISHED:
+    case LOGGED_OUT:
       return { ...state, ...payload };
   }
 };
