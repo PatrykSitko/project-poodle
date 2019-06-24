68408fe332b5d491c433b47a9d300f81e3ece64a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-24 14:10:06 +0200

    Included groups to default export

--- src/redux/actions/all.js
+++ src/redux/actions/all.js
@@ -1,10 +1,12 @@
 import student from "./fetch/student";
 import session from "./fetch/session";
+import groups from "./fetch/instructor/groups";
 import windowInnerWidth from "./window/inner/width";
 import windowInnerHeight from "./window/inner/height";
 import mousedown from "./mouse/down";
 
 export default {
+  groups,
   student,
   session,
   windowInnerWidth,
