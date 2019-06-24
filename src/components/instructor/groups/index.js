b716881eb0c35e3432d58e84ad9887c6b763a672
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-06-24 14:05:14 +0200

    Created a new Groups Component

---
+++ src/components/instructor/groups/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ImageButton from "../../button/image";
+import Group from "../../group";
+import Member from "../../group/member";
+import javaLogo from "../../../images/java-logo.png";
+import javaIotLogo from "../../../images/java-iot-logo.png";
+import dotNetLogo from "../../../images/dotnet-logo.png";
+import pcLogo from "../../../images/pc-logo.png";
+import { connect } from "react-redux";
+import "./groups.css";
+
+const mapStateToProps = ({ groups, windowInnerWidth: { value } }) => {
+  return { groups, windowInnerWidth: value };
+};
+export function Groups({ filter, groups, windowInnerWidth }) {
+  const members =
+    groups &&
+    Object.values(groups).map(({ groupName }, index) => {
+      let image = "";
+      if (groupName.toLowerCase().includes("java")) {
+        if (groupName.toLowerCase().includes("iot")) {
+          image = javaIotLogo;
+        } else {
+          image = javaLogo;
+        }
+      }
+      if (groupName.toLowerCase().includes("dotnet")) {
+        image = dotNetLogo;
+      }
+      if (groupName.toLowerCase().includes("pc")) {
+        image = pcLogo;
+      }
+      return (
+        <Member key={index}>
+          <ImageButton image={image} go="/student/student">
+            {groupName}
+          </ImageButton>
+        </Member>
+      );
+    });
+  return (
+    <Group
+      key="groups"
+      globalStyle={{ margin: 3 }}
+      globalClassName="groups-member"
+      columns={[[1, 500], [2, 1000], [3, 1500]]}
+      /* columns={Math.floor(windowInnerWidth / 500)}*/
+      filters={filter}
+      className="groups"
+    >
+      {members}
+      <Member key="-1" />
+      <Member key="-2" />
+      <Member key="-3" />
+    </Group>
+  );
+}
+
+export default connect(mapStateToProps)(Groups);
